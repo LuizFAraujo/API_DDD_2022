47a97df38f8586f6d983bbd8c621fe3eac85cc0d
LuizFAraujo <luizfaraujo.eng@gmail.com>
2022-10-10 22:31:16 -0300

    Implementação TOKEN.

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,4 +1,76 @@
+using Aplicacao.Aplicacoes;
+using Aplicacao.Interfaces;
+using Dominio.Interfaces;
+using Dominio.Interfaces.Genericos;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using Infraestrutura.Configuracoes;
+using Infraestrutura.Repositorio;
+using Infraestrutura.Repositorio.Genericos;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Token;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// =================================================================
+//Contexto criado (Banco de Dados):
+builder.Services.AddDbContext<Contexto>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+    .AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<Contexto>();
+
+// =================================================================
+// INTERFACE E REPOSITÓRIO
+builder.Services.AddSingleton(typeof(IGenericos<>), typeof(RepositorioGenerico<>));
+builder.Services.AddSingleton<INoticia, RepositorioNoticia>();
+builder.Services.AddSingleton<IUsuario, RepositorioUsuario>();
+
+// SERVIÇO DOMINIO
+builder.Services.AddSingleton<IServicoNoticia, IServicoNoticia>();
+
+// INTERFACE APLICACAO
+builder.Services.AddSingleton<IAplicacaoNoticia, AplicacaoNoticia>();
+builder.Services.AddSingleton<IAplicacaoUsuario, AplicacaoUsuario>();
+
+// TOKEN
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(option =>
+       {
+           option.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = false,
+               ValidateAudience = false,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+
+               ValidIssuer = "Teste.Securiry.Bearer",
+               ValidAudience = "Teste.Securiry.Bearer",
+               IssuerSigningKey = JwtSecurityKey.Create("Secret_Key-12345678")
+           };
+
+           // para caso de algum erro
+           option.Events = new JwtBearerEvents
+           {
+               OnAuthenticationFailed = context =>
+               {
+                   Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                   return Task.CompletedTask;
+               },
+               OnTokenValidated = context =>
+               {
+                   Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                   return Task.CompletedTask;
+               }
+           };
+       });
+
+// =================================================================
+
 
 // Add services to the container.
 
